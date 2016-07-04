557aa8f4cbeaa7cd2c7daf8cb5f588ed456c6c97
Paul Judal <pjudal@gmail.com>
2016-07-04 15:39:49 +0800

    Project Structure (left couple of files to base on)

---
+++ public/load.php
@@ -0,0 +1,36 @@
+<?php
+
+	session_start();
+
+	$proj_name = $_SESSION["proj_name"];
+	$proj_id = $_SESSION["proj_id"];
+	$assigned = $_SESSION["assigned_persons"];
+	$unassigned = $_SESSION["unassigned_persons"];
+
+	echo
+	"
+	<select id=\"assign_sel\" ng-model=\"person_id\"
+		ng-init=\"unassigned =" . htmlspecialchars(json_encode($unassigned)) . "\"
+		ng-options=\"x.id as x.first_name + x.last_name for x in unassigned\">
+	</select>
+	";
+
+	echo "<input class=\"sel_button\" type=\"submit\" value=\"Add\"
+		ng-click=\"assign(person_id, " . $proj_id . ")\"
+			>";
+	echo "</form>";
+
+
+	echo "<h2 class=\"second_h2\">Current Members</h2>";
+	echo
+	"
+	<table id=\"current_members\"
+		ng-init=\"names =" . htmlspecialchars(json_encode($assigned)) . "\">
+	<tr ng-repeat=\"x in names\">
+		<td class=\"hidden\">{{x.id}}</td>
+		<td>{{x.first_name}} {{x.last_name}}</td>
+		<td><a href=\"/projects/assign\" ng-click=\"remove(x.id, " . $proj_id . ")\">Remove</a></td>
+	</tr>
+	</table>
+	";
+?>
